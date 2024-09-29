91ed6357f5f9ac74ea1da5ba7905d3dd14eed67b
Abizrh <abizarah857@gmail.com>
2024-09-29 10:03:50 +0700

    feat: add moving emoji's as sprites

--- lua/beastie/beastie.lua
+++ lua/beastie/beastie.lua
@@ -1,7 +1,59 @@
+local ui = require('beastie.ui')
+local config = require('beastie.config')
+
+
+---@class Beastie
+---@field current_frame string
+---@field frame_index integer
+---@field frames table
+---@field animation_speed integer
+---@field start_at_launch boolean
 local beastie = {}
 
--- @param opts table
+beastie.current_frame = nil
+beastie.frame_index = 1
+
+
+function beastie.update_frame()
+  beastie.frame_index = beastie.frame_index + 1
+  if beastie.frame_index > #config.options.frames then
+    beastie.frame_index = 1
+  end
+  beastie.current_frame = config.options.frames[beastie.frame_index]
+end
+
+function beastie.start_beastie()
+  beastie.current_frame = config.options.frames[beastie.frame_index]
+  beastie.timer = vim.loop.new_timer()
+  beastie.timer:start(0, config.options.animation_speed, vim.schedule_wrap(function()
+    beastie.update_frame()
+    ui.updateUI(beastie)
+  end))
+end
+
+function beastie.stop_beastie()
+  if beastie.timer then
+    beastie.timer:stop()
+    beastie.timer:close()
+    beastie.timer = nil
+  end
+end
+
+---@class BeastieOpts
+---@field frames table
+---@field animation_speed integer
+---@field start_at_launch boolean
+
+---@param opts BeastieOpts
 function beastie.setup(opts)
+  config.setup(opts)
+
+  vim.api.nvim_create_user_command('StartBeastie', beastie.start_beastie, {})
+  vim.api.nvim_create_user_command('StopBeastie', beastie.stop_beastie, {})
+
+  if beastie.start_at_launch then
+    beastie.start_beastie()
+  end
 end
 
 return beastie
--- lua/beastie/ui.lua
+++ lua/beastie/ui.lua
@@ -1,3 +1,40 @@
+local config = require('beastie.config')
+-- @return table
+local function createBufferOpts()
+  local opts = {
+    relative = 'editor',
+    width = 3,
+    height = 1,
+    row = 5,
+    col = 5,
+    style = 'minimal'
+  }
+  return opts
+end
+
+local function createBufferUi()
+  local buffer = vim.api.nvim_create_buf(false, true)
+  -- apply_buffer_keymaps(buffer)
+  return buffer
+end
+
+
+---@class BeastieUI
+---@field buffer integer
+---@field buffer_opts table
 local UI = {}
+UI.buffer = createBufferUi()
+UI.buffer_opts = createBufferOpts()
+
+---@param beastie Beastie
+function UI.updateUI(beastie)
+  UI.buffer_opts.col = UI.buffer_opts.col + 1
+  if UI.buffer_opts.col > vim.o.columns - 5 then
+    UI.buffer_opts.col = 5
+  end
+
+  vim.api.nvim_buf_set_lines(UI.buffer, 0, -1, false, { config.options.frames[beastie.frame_index] })
+  vim.api.nvim_open_win(UI.buffer, false, UI.buffer_opts)
+end
 
 return UI
